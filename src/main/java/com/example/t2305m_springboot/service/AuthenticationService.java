153aed1df83cc4732856eee10c1fbb6e1d2212fe
quanghoatrinh <quanghoa.trinh@gmail.com>
2025-02-11 11:00:34 +0700

    demo auth - login

--- src/main/java/com/example/t2305m_springboot/service/AuthenticationService.java
+++ src/main/java/com/example/t2305m_springboot/service/AuthenticationService.java
@@ -1,9 +1,12 @@
 package com.example.t2305m_springboot.service;
 
+import com.example.t2305m_springboot.dto.req.LoginUser;
 import com.example.t2305m_springboot.dto.req.RegisterUser;
 import com.example.t2305m_springboot.entity.User;
 import com.example.t2305m_springboot.repository.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -25,5 +28,18 @@ public class AuthenticationService {
         user.setFullName(input.getFullName());
         user.setPassword(passwordEncoder.encode(input.getPassword()));
         return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUser input){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+        User user = userRepository.findByEmail(input.getEmail());
+        if(user == null)
+            throw new UsernameNotFoundException("Email or Password is not correct.");
+        return user;
     }
 }
